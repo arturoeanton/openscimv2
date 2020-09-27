27ed2fdfc3dbb41d7f2fe38b2abc919ac97089d2
arturoeanton <arturoeanton@gmail.com>
2020-09-27 01:25:52 -0300

    gitignore

---
+++ src/main/java/com/arturoeanton/openscimv2/Openscimv2Application.java
@@ -0,0 +1,13 @@
+package com.arturoeanton.openscimv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openscimv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Openscimv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturoeanton/openscimv2/Openscimv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturoeanton.openscimv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Openscimv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
